3da8ed05d31a0830f581cde740e33576be317a5b
duythai010305@gmail.com <duythai010305@gmail.com>
2022-11-07 12:46:32 +0700

    a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            //thai dep trai
             //abc
             Console.WriteLine("Hello sssssWorld!");
         }
